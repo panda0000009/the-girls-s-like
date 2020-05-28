8db35746b537e7fc4de8105a35f0dcad4296fc6e
panda0000009 <1104747986@qq.com>
2020-05-28 23:08:36 +0800

    Add files via upload

---
+++ MiniProgram/app.js
@@ -0,0 +1,55 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    wx.getUserInfo({
+      complete: (res) => {console.log(res)},
+    })
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  onShow : function (options){
+    //判断小程序的进入方式
+    console.log(options)
+    switch(options.scene){
+          case 1001:
+            break;
+          case 1005:
+            break;
+          case 1007:
+            break;
+    }
+  },
+  globalData: {
+    title:"微信",
+    name:"小强",
+    age:"19"
+  }
+})
